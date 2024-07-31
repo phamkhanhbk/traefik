9ddde614616fbc4d28ce428a2ce1f17888bb1cb1
phamkhanhbk <phamkhanhbk@gmail.com>
2024-07-31 07:20:27 +0700

    Update change_domain.sh

--- change_domain.sh
+++ change_domain.sh
@@ -1,4 +1,5 @@
 read -p "Enter hostname: " hostname
 read -p "Enter email address: " email
-sed -i 's/traefik.sample.com/$hostname/g' docker-compose.yaml
-sed -i 's/admin@sample.com/$email/g' data/traefik.yml
+
+sed -i "s/traefik.sample.com/${hostname}/g" docker-compose.yaml
+sed -i "s/admin@sample.com/${email}/g" data/traefik.yml
