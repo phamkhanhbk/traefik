4a55d2f2527d376a0a8564799d32a5fc1d2a5908
phamkhanhbk <phamkhanhbk@gmail.com>
2024-08-03 22:08:22 +0700

    Update add_hosts.sh

--- add_hosts.sh
+++ add_hosts.sh
@@ -5,9 +5,10 @@ append_to_config() {
   local router_str="$1"
   local service_str="$2"
 
-  # Converts multi-line strings for compatibility with sed commands
+  # Convert multi-line strings for compatibility with sed commands
   local escaped_router_str
   escaped_router_str=$(echo "$router_str" | sed ':a;N;$!ba;s/\n/\\n/g')
+
   local escaped_service_str
   escaped_service_str=$(echo "$service_str" | sed ':a;N;$!ba;s/\n/\\n/g')
 
@@ -16,31 +17,31 @@ append_to_config() {
   sed -i "s|# Add services here|$escaped_service_str|" data/config.yml
 }
 
-# Start the while loop
+# Main
+
 while true; do
   read -p "Enter Router Name: " router
   read -p "Enter Host name: " hostname
-  read -p 'Enter "IP address:Port":' ipaddr
+  read -p 'Enter "IP address:Port": ' ipaddr
 
-  # Define multi-line strings using both single and double quotes
-  router_str='    '"$router"':
+  router_str="    $router:
       entryPoints:
-        - "https"
-      rule: "Host(`'"$hostname"'`)"
+        - \"https\"
+      rule: \"Host(\`${hostname}\`)\"
       middlewares:
         - default-headers
         - https-redirectscheme
       tls:
         certResolver: http
-      service: '"$router"'
-# Add routers here'
+      service: $router
+# Add routers here"
 
-  service_str='    '"$router"':
+  service_str="    $router:
       loadBalancer:
         servers:
-          - url: "http://'"$ipaddr"'"
+          - url: \"http://${ipaddr}\"
         passHostHeader: true
-# Add services here'
+# Add services here"
 
   # Append configuration to the file
   append_to_config "$router_str" "$service_str"
