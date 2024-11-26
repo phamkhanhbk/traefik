4a099d3b53c8ab299fed21d85fab0d1ac9224e71
phamkhanhbk <phamkhanhbk@gmail.com>
2024-11-26 08:26:06 +0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -10,6 +10,6 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker $USER && newgrp docker
 docker --version
 docker-compose --version
-sudo usermod -aG docker $USER && newgrp docker
